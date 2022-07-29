f735fd7ce9d66fb42917f00850c1c2f6cebffcdf
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2022-07-29 21:39:39 +0200

    Added method declarations to Graph header file.

--- Graph/Graph.hpp
+++ Graph/Graph.hpp
@@ -1,12 +1,33 @@
-#include <iostream>
 #ifndef __GRAPH_H_CHRISPS
 #define __GRAPH_H_CHRISPS
+#include <iostream>
+#include <memory>
+#include <vector>
 template <class V, class E>
 class Graph
 {
+    class Edge : E
+    {
+        int _vertex;
+
+    public:
+        Edge(int, E);
+    };
+
+    class Vertex : V, std::vector<E>
+    {
+    };
+
+    std::shared_ptr<std::vector<V>> _graph;
+
 public:
     Graph();
+    Graph(int);
     ~Graph();
+
+    void edge_dir(int, int, E);
+    void edge_bidir(int, int, E);
+    friend std::ofstream &operator<<(std::ofstream &, Graph<V, E>);
 };
 #include "Graph.cpp"
 #endif
